77f7f62d5102b8210e49aca0db6fd34cccf7ee0d
= <=>
2021-02-21 15:16:00 -0800

    Added conditioning code to prep data frames for animated plot

--- dataimports.py
+++ dataimports.py
@@ -1,7 +1,31 @@
 import pandas as pd
 
-# Home Ownership Data
+# Home Ownership Data, from US Census B
 
-homeownershipbystate = pd.read_csv("data/yearlyhousingdatabystate.txt", delim_whitespace=True)
+df = pd.read_csv("data/yearlyhousingdatabystate.txt", delim_whitespace=True)
 
-print(homeownershipbystate.head())
+dftx = pd.DataFrame(columns=['state', 'year', 'percent'])
+
+#get column header list
+yearlist = list(df.keys())
+print(yearlist)
+#take state column out
+del yearlist[0]
+print(yearlist)
+
+#get each state
+statelist = df['STATE'].tolist()
+print(statelist)
+
+# with each state, get all year data and add entries for each
+entrylist = [['STATE', 'YEAR', 'PERCENT']]
+for state in statelist:
+
+    percforstate = df.query("STATE == '" + state + "'")
+
+    for year in yearlist:
+        percforyear = percforstate[year].tolist()[0]
+        entrylist.append([state, year, percforyear])
+
+entriesbystate = pd.DataFrame(data=entrylist)
+entriesbystate.to_csv("data/housingdataconditioned.csv")
